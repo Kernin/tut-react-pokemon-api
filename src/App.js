28839b40c9d603b91197ee8f5259005778891bb8
Kernin <jorgezbrito@gmail.com>
2022-09-08 22:30:31 -0500

    nuevo branch commi

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Pokemon API
         </a>
       </header>
     </div>
