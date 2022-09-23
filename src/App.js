4247ff915093510a67d79054ccc8647a0b2ab0a1
Kernin <jorgezbrito@gmail.com>
2022-09-23 18:14:21 -0500

    working on an error not showing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import axios from 'axios';
+
+
+// axios.get('https://pokeapi.co/api/v2/evolution-chain/')
+//   .catch(error => console.log(error))
+//   .then(response => console.log(response))
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This will be a pokemon app
-        </p>
-        <a
-          className="App-link"
+        <h1>
+          This will be Pokemon app
+        </h1>
+        <a className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React with Pokemon API
         </a>
       </header>
